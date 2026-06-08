6658e49a47d7cc31cf5db5de5797359f15fe6f30
phucvuong1707-sys <phucvuong1707@gmail.com>
2026-06-08 23:04:05 +0700

    Nộp bài hoàn thiện Form Guarantee

--- GUI/obj/Debug/net9.0-windows/GUI.AssemblyInfo.cs
+++ GUI/obj/Debug/net9.0-windows/GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55532418a05f4da285d3ba2163e86081cca0fd00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e675be24e95cacd2f65f4bbc44686af141c0e54")]
 [assembly: System.Reflection.AssemblyProductAttribute("GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
